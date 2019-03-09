45195460512ef8d6b09f8551240b476e102e1b2d
Carlos Ponce <jcponce510@gmail.com>
2019-03-08 21:57:36 -0800

    Initialize Firebase.

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  // Initialize Firebase.
+  var config = {
+    apiKey: "AIzaSyACBhEF9b-ulQqr5E3MrGOnY3iNu1pSkJQ",
+    authDomain: "farley-foo.firebaseapp.com",
+    databaseURL: "https://farley-foo.firebaseio.com",
+    projectId: "farley-foo",
+    storageBucket: "farley-foo.appspot.com",
+    messagingSenderId: "808010250885"
+  };
+  firebase.initializeApp(config);
+
+});
