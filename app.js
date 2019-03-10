94bb9e7052daf861e177b37aabf20255b8fef795
Carlos Ponce <jcponce510@gmail.com>
2019-03-09 20:45:49 -0800

    Add functionality to submit button.

--- app.js
+++ app.js
@@ -11,4 +11,46 @@ $(document).ready(function() {
   };
   firebase.initializeApp(config);
 
+  var database = firebase.database();
+
+  // Button for adding trains.
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    // Capture user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var firstTrainTime = moment($("#first-train-time-input").val().trim(), "MM/DD/YYYY").format("X");
+    var trainFreq = $("#frequency-input").val().trim();
+
+    // Create object for holding train data.
+    var newTrain = {
+      name: trainName,
+      destination: trainDest,
+      start: firstTrainTime,
+      frequency: trainFreq
+    };
+
+    // Upload train data to database.
+    database.ref().push(newTrain);
+
+    // Log to console.
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.start);
+    console.log(newTrain.frequency);
+
+    alert("Train successfully added");
+
+    // Clear all input boxes.
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-time-input").val("");
+    $("#frequency-input").val("");
+  });
+
+  // Create Firebase event for adding train to database and new row to HTML
+  // when new train is submitted.
+
+
 });
