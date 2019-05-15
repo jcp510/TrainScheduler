d39cab56b76ea17693cde9840fe6aa6ac64de10d
Carlos Ponce <jcponce510@gmail.com>
2019-05-15 12:04:54 -0700

    Initialize firebase.

--- public/app.js
+++ public/app.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
     databaseURL: "https://farley-foo.firebaseio.com",
     projectId: "farley-foo",
     storageBucket: "farley-foo.appspot.com",
-    messagingSenderId: "808010250885"
+    messagingSenderId: "808010250885",
+    appId: "1:808010250885:web:9dc25da2104964fc"
   };
   firebase.initializeApp(config);
 
@@ -51,6 +52,45 @@ $(document).ready(function() {
 
   // Create Firebase event for adding train to database and new row to HTML
   // when new train is submitted.
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
 
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var firstTrainTime = childSnapshot.val().start;
+    var trainFreq = childSnapshot.val().frequency;
+
+    // Log train info.
+    console.log(trainName);
+    console.log(trainDest);
+    console.log(firstTrainTime);
+    console.log(trainFreq);
+
+    // Prettify train start
+    var trainStartPretty = moment.unix(firstTrainTime).format("MM/DD/YYYY");
+
+    // Calculate the months worked using hardcore math
+    // To calculate the months worked
+    var empMonths = moment().diff(moment(empStart, "X"), "months");
+    console.log(empMonths);
+
+    // Calculate the total billed rate
+    var empBilled = empMonths * empRate;
+    console.log(empBilled);
+
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDest),
+      $("<td>").text(trainFreq),
+      $("<td>").text(empMonths),
+      $("<td>").text(empRate),
+      $("<td>").text(empBilled)
+    );
+
+    // Append the new row to the table
+    $("#employee-table > tbody").append(newRow);
+  });
 
 });
